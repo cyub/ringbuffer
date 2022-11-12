732d54b26eb74e1189b19dd3f45ef630a85a6c61
tink <qietingfy@gmail.com>
2022-11-12 22:12:27 +0800

    add mpsc test

---
+++ mpsc_test.go
@@ -0,0 +1,41 @@
+// Copyright 2022 tink <qietingfy@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package ringbuffer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMPSCRingBuffer(t *testing.T) {
+	cap := 16
+	spsc := NewMpscRingBuffer(cap)
+
+	assert.Equal(t, spsc.Length(), 0)
+	assert.Equal(t, spsc.Capacity(), cap)
+
+	for i := 0; i < cap*2; i++ {
+		err := spsc.Enqueue(i)
+		if i < cap {
+			assert.ErrorIs(t, err, nil)
+		} else {
+			assert.ErrorIs(t, err, ErrIsFull)
+		}
+	}
+
+	assert.Equal(t, spsc.Length(), cap)
+	assert.Equal(t, spsc.Capacity(), cap)
+	for i := 0; i < cap*2; i++ {
+		_, err := spsc.Dequeue()
+		if i < cap {
+			assert.ErrorIs(t, err, nil)
+		} else {
+			assert.ErrorIs(t, err, ErrIsEmpty)
+		}
+	}
+	assert.Equal(t, spsc.Length(), 0)
+	assert.Equal(t, spsc.Capacity(), cap)
+}
